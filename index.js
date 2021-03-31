de09454935fddcd602fd32890f635195a514ebc7
keenanjon <jon.menna@metropolia.fi>
2021-03-31 09:26:54 +0300

    Varmuus

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const catRouter = require('./routes/catRouter');
-const userRouter = require('./routes/userRouter')
+const userRouter = require('./routes/userRouter');
 
 app.use(express.static('public'));
 app.use(express.static('uploads'));
