0a645e86b970275a0b9cc109806c9fe91acf2a86
keenanjon <jon.menna@metropolia.fi>
2021-04-09 10:27:56 +0300

    trying to update differences from teachers versions

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const rootRoute = require('./routes/rootRoute');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
+const passport = require('./utils/pass');
+const authRoute = require('./routes/authRoute');
 const app = express();
 const port = 3000;
 
@@ -16,8 +18,8 @@ app.use(express.static('week2_public_html'));
 app.use(express.static('uploads'));
 
 // routes
-// app.use('/', rootRoute);
-app.use('/cat', catRoute);
-app.use('/user', userRoute);
+app.use('/auth', authRoute);
+app.use('/cat',passport.authenticate('jwt', {session: false}), catRoute);
+app.use('/user',passport.authenticate('jwt', {session: false}), userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
