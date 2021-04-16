0c2c3e71af6e3b515ece8662bd97a096801cc3d2
keenanjon <jon.menna@metropolia.fi>
2021-04-16 11:42:55 +0300

    index.js with https ready for localhost and production

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.HTTP_PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./utils/production')(app, port);
+} else {
+  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
+}
+
+
 const catRouter = require('./routes/catRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -12,14 +22,21 @@ app.use(express.static('uploads'));
 app.use(express.json()); // for parsing application/json
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+/*
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+ */
 app.use('/cat', catRouter);
 app.use('/user', userRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello Secure World!');
+});
+
+
+/*
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+ */
