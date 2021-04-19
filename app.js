869925488c5db7584c6494743dce184a9d3b7f85
keenanjon <jon.menna@metropolia.fi>
2021-04-19 20:43:04 +0300

    8000 changes

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+//const port = 3000;
 
 
-/*
 const port = process.env.HTTP_PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV === 'production') {
@@ -16,7 +15,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
 }
- */
 
 
 const rootRoute = require('./routes/rootRoute');
@@ -39,4 +37,4 @@ app.use('/auth', authRoute);
 app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
